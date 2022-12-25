9e159f8b85548ca80d2c32db51778be15eb78bc0
Nicholai <n.paz4444@gmail.com>
2022-12-25 10:45:07 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int exampleOne = 1;
+        int exampleTwo = 16;
+        int exampleThree = 3;
+
+        System.out.println("Example One : " + solution.isPowerOfTwo(exampleOne));
+        System.out.println("Example Two : " + solution.isPowerOfTwo(exampleTwo));
+        System.out.println("Example Three : " + solution.isPowerOfTwo(exampleThree));
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        long i = 1;
+        while (i < n) {
+            i *= 2;
+        }
+        return i == n;
+    }
+}
